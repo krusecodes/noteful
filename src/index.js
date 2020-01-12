062df7e14b43bd8abcac76bc312eeaea8523f55f
Noah Kruse <noah.j.kruse@biola.edu>
2020-01-12 13:15:37 -0800

    refactored app to alter components using context

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
-import App from './App/App';
-import './index.css';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { BrowserRouter } from 'react-router-dom'
+
+import './index.css'
+import App from './App/App'
 
 ReactDOM.render(
     <BrowserRouter>
